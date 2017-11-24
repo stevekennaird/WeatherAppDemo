0b9b69fb9eaedea55a910b4ce03a99561dc5de5a
Steve Kennaird <hello@stevekennaird.com>
2017-11-24 17:57:43 +0000

    Making days selectable

--- src/Components/DayList.js
+++ src/Components/DayList.js
@@ -3,14 +3,29 @@ import weatherForecast from "../Data/weather.json";
 import DayListItem from "./DayListItem";
 
 class DayList extends Component {
+  constructor() {
+    super();
+    this.state = { selectedIndex: 0 };
+  }
+
+  handleDayClick = newIndex => {
+    this.setState({ selectedIndex: newIndex });
+  };
+
   render() {
-    const days = weatherForecast.Days.map(day => (
-      <DayListItem dayOfWeek={day.DayOfWeek} summary={day.ShortSummary} />
+    const days = weatherForecast.Days.map((day, index) => (
+      <DayListItem
+        key={index}
+        dayOfWeek={day.DayOfWeek}
+        summary={day.ShortSummary}
+        selected={index === this.state.selectedIndex}
+        onclick={() => this.handleDayClick(index)}
+      />
     ));
 
     return (
       <div>
-        <h3>This week's weather:</h3>
+        <h3>Select a day to view an hourly forecast:</h3>
         <div className="dayListContainer">{days}</div>
       </div>
     );
--- src/Components/DayListItem.js
+++ src/Components/DayListItem.js
@@ -4,7 +4,10 @@ import WeatherSummaryIcon from "./WeatherSummaryIcon";
 class DayListItem extends Component {
   render() {
     return (
-      <div className="day">
+      <div
+        className={this.props.selected ? "day selected" : "day"}
+        onClick={this.props.onclick}
+      >
         <h3>{this.props.dayOfWeek.substring(0, 3)}</h3>
         <WeatherSummaryIcon summary={this.props.summary} />
       </div>
